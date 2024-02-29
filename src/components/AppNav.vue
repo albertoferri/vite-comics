ac57f71eb884ad49f92014cf4ac9527b5bb3e49b
albertoferri <falberto99@gmail.com>
2024-02-29 14:24:40 +0100

    aggiunta navbar

--- src/components/AppNav.vue
+++ src/components/AppNav.vue
@@ -8,11 +8,64 @@ export default {
 
 <!-- --------QUI VA L'HTML-------- -->
 <template>
-    <div id="navbar">
-        
-    </div>
+  <div class="container">
+    <nav>
+        <img src="/img/dc-logo.png" alt=" DC logo">
+        <ul>
+          <li>characters</li>
+          <li>comics</li>
+          <li>movies</li>
+          <li>tv</li>
+          <li>games</li>
+          <li>collectibles</li>
+          <li>videos</li>
+          <li>fans</li>
+          <li>news</li>
+          <li>shop</li>
+        </ul>
+    </nav>
+  </div>
 </template>
 
 <style lang="scss">
+
+.container{
+  background-color: white;
+
+  nav{
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    width: 1200px;
+    height: 120px;
+    margin: 0 auto;
+
+    img{
+      height: 80px;
+      width: auto;
+      cursor: pointer;
+    }
+
+    ul{
+      display: flex;
+      gap: 30px;
+      padding: 0;
+    }
+
+    li{
+      list-style-type: none;
+      font-weight: bold;
+      text-transform: uppercase;
+      font-size: 12px;
+      cursor: pointer;
+    }
+
+  }
+
+}
+
+
+
 
 </style>
