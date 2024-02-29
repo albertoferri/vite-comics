39d68145f3947edb7fdc395c40a7fa2d7865a875
albertoferri <falberto99@gmail.com>
2024-02-29 14:04:34 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script>
+import AppNav from './components/AppNav.vue';
+
+export default{
+  components:{
+    AppNav,
+  }
+}
+
+</script>
+
+<template>
+
+  <AppNav></AppNav>
+
+</template>
+
+<style>
+
+</style>
---
+++ src/components/AppNav.vue
@@ -0,0 +1,18 @@
+<!-- --------QUI VA I TUOI SCRIPT JS LEGATI A QUESTO COMPONENTS IN PARTICOLARE-------- -->
+<script>
+export default {
+  name: 'AppNav',
+}
+
+</script>
+
+<!-- --------QUI VA L'HTML-------- -->
+<template>
+    <div id="navbar">
+        
+    </div>
+</template>
+
+<style lang="scss">
+
+</style>
